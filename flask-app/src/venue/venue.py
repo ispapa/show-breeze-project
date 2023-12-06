65a62149cb138dce69c78e7ed144e943328e9a75
Jonathan Yu <yu.jona@northeastern.edu>
2023-12-05 19:58:57 -0500

    Set up the blueprint prefix

--- flask-app/src/venue/venue.py
+++ flask-app/src/venue/venue.py
@@ -5,7 +5,7 @@ from src import db
 
 venue = Blueprint('venue', __name__)
 
-# Get all customers from the DB
+# Get all venues from the DB
 @venue.route('/venue', methods=['GET'])
 def get_venue():
     cursor = db.get_db().cursor()
@@ -20,9 +20,51 @@ def get_venue():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get customer detail for customer with particular userID
+# Adds a new Venue to the database
+@venue.route('/venue', methods=['POST'])
+def add_new_venue():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variable=
+    name = the_data['venue_name']
+    capacity = the_data['venue_capacity']
+    startTime = the_data['venue_start']
+    endTime = the_data['venue_end']
+    price = the_data['venue_price']
+    street = the_data['venue_street']
+    city = the_data['venue_city']
+    state = the_data['venue_state']
+    zip = the_data['venue_zip']
+    ownerId = the_data['venue_ownerId']
+
+    # Constructing the query
+    query = 'insert into products (name, capacity, startTime, endtime, price, street, city, state, zip, ownerId) values ("'
+    query += name + '", "'
+    query += str(capacity) + '", "'
+    query += startTime + '", '
+    query += endTime + '", '
+    query += str(price) + '", '
+    query += street + '", '
+    query += city + '", '
+    query += state + '", '
+    query += zip + '", '
+    query += str(ownerId) + '", '')'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+    
+    return 'Success!'
+
+
+# Get venue detail for Venues with particular userID
 @venue.route('/venue/<userID>', methods=['GET'])
-def get_customer(userID):
+def get_venue(userID):
     cursor = db.get_db().cursor()
     cursor.execute('select * from Venues where id = {0}'.format(userID))
     row_headers = [x[0] for x in cursor.description]
@@ -34,3 +76,44 @@ def get_customer(userID):
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
     return the_response
+
+# Updates all the data for the venueId with the request.json,
+# Ideally if some data does not change the request.json will have it unchanged
+@venue.route('/venue/<userID>', methods=['PUT'])
+def update_venue(venueId):
+    #Getting the Data
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    #extracting the variables
+    name = the_data['venue_name']
+    capacity = the_data['venue_capacity']
+    startTime = the_data['venue_start']
+    endTime = the_data['venue_end']
+    price = the_data['venue_price']
+    street = the_data['venue_street']
+    city = the_data['venue_city']
+    state = the_data['venue_state']
+    zip = the_data['venue_zip']
+    ownerId = the_data['venue_ownerId']
+    venueId = the_data['venueId']
+    
+    # update price + user id 
+    venue_update = 'UPDATE `Venues` SET name = ' + name
+    + ', capacity = ' + capacity
+    + ', startTime = ' + startTime
+    + ', endTime = ' + endTime
+    + ', price = ' + str(price) 
+    + ', street = ' + street
+    + ', city = ' + city
+    + ', state = ' + state
+    + ', zip = ' + zip 
+    + ', ownerId = ' + str(ownerId)  
+    + ', venueId = ' + str(venueId)  
+    + ' WHERE ticketId = ' + str(venueId) + ';'
+
+    cursor = db.get_db().cursor()
+    cursor.execute(venue_update)
+    db.get_db().commit()
+
+    return "successfully updated venue #{0}!".format(venueId)
