beefefd01e21f8b2cd16c46f6e6a08f613bc2da1
ispapa <belle07704@gmail.com>
2023-12-05 19:55:59 -0500

    route fixes

--- flask-app/src/tickets/tickets.py
+++ flask-app/src/tickets/tickets.py
@@ -23,8 +23,8 @@ def get_tickets():
         JOIN Events e ON t.eventId = e.eventId
         JOIN Venues v ON e.venueId = v.venueId
         JOIN Seats s ON t.seatId = s.seatId
-        WHERE t.userId = 21
-    ''')
+        WHERE t.userId = 21;
+        ''' )
 
     row_headers = [x[0] for x in cursor.description]
     json_data = []
@@ -40,20 +40,6 @@ def get_tickets():
     return response
 
 
-# Get ticket detail for ticket with particular ticketId
-@tickets.route('/tickets/<ticketId>', methods=['GET'])
-def get_ticket(ticketId):
-    cursor = db.get_db().cursor()
-    cursor.execute('select * from Tickets where ticketId = {0}'.format(ticketId))
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    the_response.mimetype = 'application/json'
-    return the_response
 
 # Changes userId and price of ticket (for resale transaction)
 @tickets.route('/editTicket/<ticketId>', methods=['PUT'])
@@ -62,20 +48,19 @@ def update_ticket(ticketId):
     the_data = request.json
 
     ticketId = the_data['ticketId']
-    new_user_id = the_data['buyerId']
+    buyerEmail = the_data['buyerEmail']
     new_price = the_data['price']
-    
-    ticketInfo = get_ticket(ticketId)
-    
-    prev_price = str(ticketInfo.json[0].get('price'))
-    
-    # calculate price change (if any)
-    price_change = float(new_price) - float(prev_price)
-    
-    # update price + user id 
-    ticket_query = 'UPDATE `Tickets` SET userId = ' + str(new_user_id) + ', price = price + ' + str(price_change) + ' WHERE ticketId = ' + str(ticketId) + ';'
 
+    # Get buyer ID
     cursor = db.get_db().cursor()
+    cursor.execute('SELECT userId FROM Users WHERE email = %s', (buyerEmail,))
+    buyerId = cursor.fetchone()[0]
+    if not buyerId:
+        return 'Buyer not found!', 404
+
+    # update price + user id 
+    ticket_query = 'UPDATE Tickets SET userId = ' + str(buyerId) + ', price =  ' + str(new_price) + ' WHERE ticketId = ' + str(ticketId) + ';'
+
     cursor.execute(ticket_query)
     db.get_db().commit()
 
--- flask-app/src/transactions/transactions.py
+++ flask-app/src/transactions/transactions.py
@@ -11,10 +11,26 @@ def add_new_transaction():
     the_data = request.json
 
     #extracting the variable
-    sellerId = the_data['sellerId']
-    buyerId = the_data['buyerId']
+    sellerEmail = the_data['sellerEmail']
+    buyerEmail = the_data['buyerEmail']
     ticketId = the_data['ticketId']
     price = the_data['price']
+
+    
+    # Querying user IDs based on emails
+    cursor = db.get_db().cursor()
+
+    # Get seller ID
+    cursor.execute('SELECT userId FROM Users WHERE email = %s', (sellerEmail,))
+    sellerId = cursor.fetchone()[0]
+    if not sellerId:
+        return 'Seller not found!', 404
+
+    # Get buyer ID
+    cursor.execute('SELECT userId FROM Users WHERE email = %s', (buyerEmail,))
+    buyerId = cursor.fetchone()[0]
+    if not buyerId:
+        return 'Buyer not found!', 404
 
     # Constructing the query
     query = 'insert into Transactions (sellerId, buyerId, ticketId, date, price) values ("'
@@ -25,7 +41,6 @@ def add_new_transaction():
     query += str(price) + ')'
 
     # executing and committing the insert statement 
-    cursor = db.get_db().cursor()
     cursor.execute(query)
     db.get_db().commit()
     
