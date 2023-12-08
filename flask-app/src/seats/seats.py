0ead648aca2b489c793eda95c6fa6e204436de6f
ispapa <belle07704@gmail.com>
2023-12-07 21:51:23 -0500

    fix seats

--- flask-app/src/seats/seats.py
+++ flask-app/src/seats/seats.py
@@ -7,34 +7,20 @@ seats = Blueprint('seats', __name__)
 # Updates a current seat to the database
 @seats.route('/seats/<seatId>', methods=['PUT'])
 def update_seat(seatId):
-    
     # collecting data from the request object 
     the_data = request.json
     current_app.logger.info(the_data)
 
-    #extracting the variable=
-    venueId = the_data['venueId']
-    availability = the_data['availability']
-    availability_num = 0
-    if (availability) : availability_num = 1
-    seatRow = the_data['seatRow']
-    seatNumber = the_data['seatNumber']
+    seatId = the_data['Seat ID']
 
     # Constructing the query
-    seats_update = 'UPDATE `Seats` SET seatId = "' + str(seatId)
-    seats_update += '", venueId = "' + str(venueId)
-    seats_update += '", availability = "' + str(availability_num)
-    seats_update += '", seatRow = "' + seatRow
-    seats_update += '", seatNumber = "' + str(seatNumber)
-    seats_update += '" WHERE seatId = ' + str(seatId) + ';'
+    seats_update = f'UPDATE `Seats` SET availability = FALSE WHERE seatId = {seatId};'
 
-
-    # executing and committing the insert statement 
     cursor = db.get_db().cursor()
     cursor.execute(seats_update)
     db.get_db().commit()
     
-    return "Successfully updated seat with ID " + str(seatId)
+    return f"Successfully updated seat with ID {seatId}"
 
 # Deletes a venue based on the veneuId
 @seats.route('/seats/<int:seatId>', methods=['DELETE'])
